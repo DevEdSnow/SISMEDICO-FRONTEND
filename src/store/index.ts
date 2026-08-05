379a10c4c58b5e65ef46ff2fbd7a2339e081d41a
Isai Reyes <anonymus1201a@gmail.com>
2026-08-05 15:18:30 -0600

    Actualizacion de Archivo principal de Store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,17 +2,22 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./auth/authSlice";
 import medicoReducer from "./medico/medicoSlice";
+import pacienteReducer from "./paciente/pacienteSlice";
 
 export const store = configureStore({
 
     reducer: {
 
         auth: authReducer,
-        medico: medicoReducer
+
+        medico: medicoReducer,
+
+        paciente: pacienteReducer
 
     }
 
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+
 export type AppDispatch = typeof store.dispatch;

5ed8dd7e74d0e114f2085591bb30e8301806fdd6
Isai Reyes <anonymus1201a@gmail.com>
2026-08-13 18:14:21 -0600

    Creacion de Uso de de app

---
+++ src/hooks/useAppDispatch.ts
@@ -0,0 +1,8 @@
+import { useDispatch } from "react-redux";
+
+import type { AppDispatch } from "../store";
+
+const useAppDispatch =
+    () => useDispatch<AppDispatch>();
+
+export default useAppDispatch;

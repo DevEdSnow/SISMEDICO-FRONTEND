cc85257009ca3d4fd917353c4391900ff7bbc063
Isai Reyes <anonymus1201a@gmail.com>
2026-08-13 18:19:04 -0600

    Creacion del Selector de use App

---
+++ src/hooks/useAppSelector.ts
@@ -0,0 +1,17 @@
+import {
+    useSelector,
+} from "react-redux";
+
+import type {
+    TypedUseSelectorHook,
+} from "react-redux";
+
+import type {
+    RootState,
+} from "../store";
+
+const useAppSelector: TypedUseSelectorHook<
+    RootState
+> = useSelector;
+
+export default useAppSelector;

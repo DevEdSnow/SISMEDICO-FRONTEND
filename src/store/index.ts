eef14d3ebc3a80347b7134df5b2530ac77564d64
Isai Reyes <anonymus1201a@gmail.com>
2026-08-14 18:52:08 -0600

    Actualizacion de Archivo Principal de Store

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,7 @@ import usuarioReducer from "./usuario/usuarioSlice";
 import pacienteReducer from "./paciente/pacienteSlice";
 import medicoReducer from "./medico/medicoSlice";
 import citaReducer from "./cita/citaSlice";
+import diagnosticoReducer from "./diagnostico/diagnosticoSlice";
 
 export const store = configureStore({
     reducer: {
@@ -13,11 +14,9 @@ export const store = configureStore({
         paciente: pacienteReducer,
         medico: medicoReducer,
         cita: citaReducer,
+        diagnostico: diagnosticoReducer,
     },
 });
 
-export type RootState =
-    ReturnType<typeof store.getState>;
-
-export type AppDispatch =
-    typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
